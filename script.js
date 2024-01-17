85e283307cdb155eba8f3528d82fe1f2121c2bd4
Jiho Lee <121273820+jihol22@users.noreply.github.com>
2024-01-17 23:38:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,17 @@ document.getElementById('resetImages').addEventListener('click', function() {
 });
 
 let originalImages = [
-    'images/1.png',
-    'images/2.png',
-    'images/3.png',
+    'images/b1.png',
+    'images/b2.png',
+    'images/b3.png',
+    'images/b4.png',
+    'images/b5.png',
+    
+    'images/k1.png',
+    'images/k2.png',
+    'images/k3.png',
+    'images/k4.png',
+    'images/k5.png',
     
     // Add more image paths here
 ];
@@ -18,7 +26,7 @@ let images = [...originalImages];
 
 function changeImage() {
     if (images.length === 0) {
-        alert("카드가 다 끝났습니다. HOME 버튼을 눌러 재시작 해주세요!!");
+        alert("카드가 다 끝났습니다. HOME 버튼을 눌러 재시작 해주세요!");
         return;
     }
 
